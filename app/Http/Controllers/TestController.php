9a7bdffb8aa13f43a76b5462e5bc181c547a3a50
luoxiaowei <965471615@qq.com>
2018-09-12 19:58:58 +0800

    framwork

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QPF
+ * Date: 2018/9/12
+ * Time: 17:43
+ */
+
+namespace App\Http\Controllers;
+
+
+class TestController extends Controller
+{
+
+    public function show(){
+
+        return 'Hello World';
+
+    }
+
+}
